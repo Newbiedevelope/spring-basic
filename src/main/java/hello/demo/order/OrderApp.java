6702d3249289f721b0f21ad0cf9b21c02cc0a965
Newbiedevelope <rbtjr20313@gmail.com>
2026-03-10 01:43:43 +0900

    주문 및 할인 도메인 실행과 테스트

---
+++ src/main/java/hello/demo/order/OrderApp.java
@@ -0,0 +1,24 @@
+package hello.demo.order;
+
+import hello.demo.member.Grade;
+import hello.demo.member.Member;
+import hello.demo.member.MemberService;
+import hello.demo.member.MemberServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatorPrice = " + order.calculatorPrice());
+    }
+
+}
---
+++ src/test/java/hello/demo/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package hello.demo.order;
+
+import hello.demo.member.Grade;
+import hello.demo.member.Member;
+import hello.demo.member.MemberService;
+import hello.demo.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // 할인 값이 잘 적용되는지 확인
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertEquals(order.getDiscountPrice(), 1000);
+    }
+
+}
