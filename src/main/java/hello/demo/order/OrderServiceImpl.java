30d4c79f527cb1599061df763b1fbc124fbd330a
Newbiedevelope <rbtjr20313@gmail.com>
2026-03-13 12:59:03 +0900

    새로운 할인 정책 적용시 문제점(DIP 위반)

--- src/main/java/hello/demo/order/OrderServiceImpl.java
+++ src/main/java/hello/demo/order/OrderServiceImpl.java
@@ -2,14 +2,16 @@ package hello.demo.order;
 
 import hello.demo.discount.DiscountPolicy;
 import hello.demo.discount.FixDiscountPolicy;
+import hello.demo.discount.RateDiscountPolicy;
 import hello.demo.member.Member;
 import hello.demo.member.MemberRepository;
 import hello.demo.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
+    // DIP 를 지키기 위해 구현체를 생성하지 않음
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
     
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
