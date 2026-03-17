76f9f519e5bc1fb1b1c46c40ebe988970824d262
Newbiedevelope <rbtjr20313@gmail.com>
2026-03-18 01:34:12 +0900

    새로운 할인 구조 및 정책 적용(AppConfig 리팩터링을 통해 중복 제거)

--- src/main/java/hello/demo/AppConfig.java
+++ src/main/java/hello/demo/AppConfig.java
@@ -2,6 +2,7 @@ package hello.demo;
 
 import hello.demo.discount.DiscountPolicy;
 import hello.demo.discount.FixDiscountPolicy;
+import hello.demo.discount.RateDiscountPolicy;
 import hello.demo.member.MemberService;
 import hello.demo.member.MemberServiceImpl;
 import hello.demo.member.MemoryMemberRepository;
@@ -23,7 +24,10 @@ public class AppConfig {
     }
 
     private static DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy();
+        // 할인 정책 변경 시 이제 AppConfig 의 discountPolicy 메서드
+        // 구현부만 수정해주면 된다.
+        return new RateDiscountPolicy();
     }
 
 }
