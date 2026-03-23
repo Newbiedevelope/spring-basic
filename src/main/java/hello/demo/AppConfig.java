a152a099ed1c965f646a52e8639bb693cdedb4f8
Newbiedevelope <rbtjr20313@gmail.com>
2026-03-24 00:11:09 +0900

    AppConfig Spring으로 바꾸기(어노테이션 추가)

--- src/main/java/hello/demo/AppConfig.java
+++ src/main/java/hello/demo/AppConfig.java
@@ -8,22 +8,29 @@ import hello.demo.member.MemberServiceImpl;
 import hello.demo.member.MemoryMemberRepository;
 import hello.demo.order.OrderService;
 import hello.demo.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private static DiscountPolicy discountPolicy() {
+    @Bean
+    public static DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         // 할인 정책 변경 시 이제 AppConfig 의 discountPolicy 메서드
         // 구현부만 수정해주면 된다.
---
+++ src/main/java/hello/demo/OrderApp.java
@@ -0,0 +1,32 @@
+package hello.demo;
+
+import hello.demo.member.Grade;
+import hello.demo.member.Member;
+import hello.demo.member.MemberService;
+import hello.demo.order.Order;
+import hello.demo.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //OrderService orderService = appConfig.orderService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatorPrice = " + order.calculatorPrice());
+    }
+
+}
