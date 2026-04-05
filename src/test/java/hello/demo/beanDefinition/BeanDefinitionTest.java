404d302aeeb92749f4f4fbe8a1e84372059854ae
Newbiedevelope <rbtjr20313@gmail.com>
2026-04-06 03:17:22 +0900

    스프링 빈 설정 메타정보 확인하기

---
+++ src/test/java/hello/demo/beanDefinition/BeanDefinitionTest.java
@@ -0,0 +1,32 @@
+package hello.demo.beanDefinition;
+
+import hello.demo.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+    // 설정 클래스로 어플리케이션 컨텍스트 색성 - 팩토리 메서드를 활용한 AppConfig 클래스
+    //AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig .class);
+
+    // xml 설정파일로 애플리케이션 컨텍스트 생성 -
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+
+
+}
