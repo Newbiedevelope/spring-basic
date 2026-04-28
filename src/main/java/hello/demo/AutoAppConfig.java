c2d92f2991769686cc6c95fb476c3945b9fde670
Newbiedevelope <rbtjr20313@gmail.com>
2026-04-29 00:07:59 +0900

    중복등록과 충돌

--- src/main/java/hello/demo/AutoAppConfig.java
+++ src/main/java/hello/demo/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.demo;
 
+import hello.demo.member.MemberRepository;
+import hello.demo.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,5 +13,12 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 빈 이름을 수동으로 지정시 우선권을 얻게 된다.
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 
 }
